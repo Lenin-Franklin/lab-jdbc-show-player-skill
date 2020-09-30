cb69e35c5223ffefee86a9bbe0ba5ecbf1fdcd54
Lenin Franklin <leninfranklinee@gmail.com>
2020-09-30 23:01:00 +0530

    first commit

--- src/model/Skill.java
+++ src/model/Skill.java
@@ -1,1 +1,29 @@
+package model;
 
+public class Skill
+{
+	private long skilld;
+	private String skillName;
+	
+	public long getSkilld()
+	{
+		return skilld;
+	}
+	public String getSkillName()
+	{
+		return skillName;
+	}
+	public void setsKilld(Long skilld)
+	{
+		this.skilld = skilld;
+	}
+	public void setSKillName(String skillName)
+	{
+		this.skillName = skillName;
+	}
+	public Skill(Long skilld, String skillName) {
+		super();
+		this.skilld = skilld;
+		this.skillName = skillName;
+	}
+	}
